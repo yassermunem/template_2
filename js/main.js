46dc4e1e4b2ab317ddd0c9ff98ad664e48f97187
yassermunem <62205581+yassermunem@users.noreply.github.com>
2021-06-08 03:48:33 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 
+var sButton = document.getElementById('scroll');
+
+window.onscroll = function () {
+    if (window.pageYOffset >= 450) {
+        sButton.style.display = "block";
+    } else {
+        sButton.style.display = "none";
+    }
+}
+
+
+sButton.onclick = function () {
+    window.scrollTo(0, 0);
+}
